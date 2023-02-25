924c738d871e50fab6b023900e42263e076db101
Aditi Mittal <aditim26@yahoo.com>
2023-02-25 21:37:42 +0530

    full code

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function thankyou(name)
+{
+    alert("Thankyou! for the registering our team will contact you shortly ")
+}
+function review()
+{
+    alert("Thankyou! for the feedback hope you liked our service")
+}
